c4403dbdd7a0681a4247904af7a97710a9ad461c
aldorahman <rahmanaldo69@gmail.com>
2022-05-30 11:41:55 +0700

    m

--- config/config-database.go
+++ config/config-database.go
@@ -19,21 +19,21 @@ func SetupDatabaseConnection() *gorm.DB {
 		panic("failed to load file")
 	}
 
-	db_host := os.Getenv("DB_HOST")
-	db_user := os.Getenv("DB_USER")
-	db_pass := os.Getenv("DB_PASSWORD")
-	db_name := os.Getenv("DB_DATABASE")
-	db_port := os.Getenv("DB_PORT")
-	db_sslmode := os.Getenv("DB_SSLMODE")
+	// db_host := os.Getenv("DB_HOST")
+	// db_user := os.Getenv("DB_USER")
+	// db_pass := os.Getenv("DB_PASSWORD")
+	// db_name := os.Getenv("DB_DATABASE")
+	// db_port := os.Getenv("DB_PORT")
+	// db_sslmode := os.Getenv("DB_SSLMODE")
 
 	// db_user := "root"
 	// db_pass := ""
 	// db_host := "localhost"
 	// db_name := "db_golang"
 
-	// db_url := os.Getenv("DATABASE_URL")
+	dsn := os.Getenv("DATABASE_URL")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", db_host, db_user, db_pass, db_name, db_port, db_sslmode)
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", db_host, db_user, db_pass, db_name, db_port, db_sslmode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("db error connection")
